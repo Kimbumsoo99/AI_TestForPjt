558b09d9a0b8b691b41e9b44d08ad8ba53506b71
Kimbumsoo99 <show7441@naver.com>
2024-08-25 23:57:15 +0900

    diffusion: cleanup API 추가 테스팅 진행중
    - Inpainting 기반 동작

---
+++ defect-studio/cleanup-server/app/main.py
@@ -0,0 +1,69 @@
+from fastapi import FastAPI, Query, File, UploadFile, Form
+from fastapi.responses import StreamingResponse
+from io import BytesIO
+from .model import generate_cleanup
+from PIL import Image
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app = FastAPI()
+
+# CORS 설정 추가
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # React 개발 서버 주소
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메소드 허용
+    allow_headers=["*"],  # 모든 헤더 허용
+)
+
+# 이미지 저장 디렉토리
+output_dir = "C:/uploads/defect"
+os.makedirs(output_dir, exist_ok=True)
+
+@app.post("/generate-cleanup/")
+async def create_cleanup(
+        prompt: str = Form(..., description="이미지를 변환할 텍스트 프롬프트"),
+        image: UploadFile = File(..., description="기본 이미지 파일"),
+        mask: UploadFile = File(..., description="마스크 이미지 파일"),
+        num_inference_steps: int = Form(50, ge=1, le=500, description="추론 단계 수"),
+        guidance_scale: float = Form(7.5, ge=1.0, le=20.0, description="가이던스 스케일")
+):
+    print(f"prompt: {prompt}, image: {image}, mask: {mask}")
+    try:
+        # 업로드된 이미지를 PIL.Image로 변환
+        image_bytes = await image.read()
+        init_image = Image.open(BytesIO(image_bytes)).convert("RGB")
+        init_image = init_image.resize((512, 512))
+
+        # init_image 저장
+        init_image_file = os.path.join(output_dir, "init_image.png")
+        init_image.save(init_image_file)
+        print(f"init_image saved to: {init_image_file}")
+
+        # 업로드된 마스크를 PIL.Image로 변환
+        mask_bytes = await mask.read()
+        mask_image = Image.open(BytesIO(mask_bytes)).convert("RGB")
+        mask_image = mask_image.resize((512, 512))
+
+        # mask_image 저장
+        mask_image_file = os.path.join(output_dir, "mask_image.png")
+        mask_image.save(mask_image_file)
+        print(f"mask_image saved to: {mask_image_file}")
+
+        print(f"generated_image before")
+        # Cleanup 작업 수행
+        cleaned_image = generate_cleanup(init_image, mask_image, prompt, num_inference_steps, guidance_scale)
+        print(f"generated_image after")
+
+        if cleaned_image is None:
+            return {"error": "Cleanup failed"}
+
+        # 이미지 데이터를 바이트 스트림으로 변환하여 반환
+        img_io = BytesIO()
+        cleaned_image.save(img_io, format='PNG')
+        img_io.seek(0)
+        return StreamingResponse(img_io, media_type="image/png")
+    except Exception as e:
+        print(f"Error during cleanup: {e}")
+        return {"error": str(e)}
