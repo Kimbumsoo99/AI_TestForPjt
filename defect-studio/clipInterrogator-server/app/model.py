6571e484ff7280b63729ba0c2c452bed7e582efe
Kimbumsoo99 <show7441@naver.com>
2024-08-27 10:17:51 +0900

    diffusion: CLIP GPU 리소스 문제 해결
    - 해결보다는 apply_low_vram_defaults를 통해 리소스 점유율 감소

--- defect-studio/clipInterrogator-server/app/model.py
+++ defect-studio/clipInterrogator-server/app/model.py
@@ -7,8 +7,8 @@ import torch
 clip_config = Config(
     clip_model_name="ViT-L-14/openai",  # 사용할 CLIP 모델
     cache_path="./cache",                # 캐시 경로 설정 (필요한 경우)
-    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")  # GPU 사용 설정
 )
+clip_config.apply_low_vram_defaults()  # 인스턴스 메서드로 호출
 clip_interrogator = Interrogator(clip_config)
 
 # GPU 메모리를 최적화하기 위해 사용할 수 있습니다.
