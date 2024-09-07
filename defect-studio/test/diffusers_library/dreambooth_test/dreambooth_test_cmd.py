a453a66bd361ed34b6bc659631028edd9fe25048
Kimbumsoo99 <show7441@naver.com>
2024-09-07 19:41:25 +0900

    train: 학습 관련 실습 dreambooth 코드 추가 & 실습 성공
    - diffusers library 활용

--- defect-studio/test/diffusers_library/dreambooth_test/dreambooth_test_cmd.py
+++ defect-studio/test/diffusers_library/dreambooth_test/dreambooth_test_cmd.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 from dotenv import load_dotenv
+from datetime import datetime
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 # .env 파일 로드
 load_dotenv()
@@ -9,34 +13,48 @@ load_dotenv()
 model_name = os.getenv("MODEL_NAME")
 instance_dir = os.getenv("INSTANCE_DIR")
 class_dir = os.getenv("CLASS_DIR")
-output_dir = os.getenv("OUTPUT_DIR")
+base_output_dir = os.getenv("OUTPUT_DIR")
+
+# 현재 날짜와 시간을 가져와서 고유한 숫자 문자열로 설정
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+output_dir = os.path.join(base_output_dir, f"{timestamp}_cat2_dreambooth")
+
+# OUTPUT_DIR이 존재하지 않으면 생성
+os.makedirs(output_dir, exist_ok=True)
+
+project_root = os.getenv('DIFFUSERS_TRAIN_PATH')  # 실제 프로젝트 경로로 수정해주세요.
+train_script = os.path.join(project_root, "dreambooth/train_dreambooth.py")
 
 # 명령어 구성
 command = [
-    "accelerate", "launch", "../diffusers/examples/dreambooth/train_dreambooth.py",
+    "accelerate", "launch", train_script,
     "--pretrained_model_name_or_path", model_name,
     "--instance_data_dir", instance_dir,
     "--class_data_dir", class_dir,
     "--output_dir", output_dir,
     "--with_prior_preservation",
     "--prior_loss_weight", "1.0",
-    "--instance_prompt", "a photo of sks dog",
-    "--class_prompt", "a photo of dog",
+    "--instance_prompt", "a photo of sks cat",
+    "--class_prompt", "a photo of cat",
     "--resolution", "512",
-    "--train_batch_size", "1",
-    "--gradient_accumulation_steps", "1",
-    "--learning_rate", "5e-6",
+    "--train_batch_size", "16" ,
+    "--gradient_accumulation_steps", "2",
+    # 16GB add
+    "--gradient_checkpointing",
+    "--use_8bit_adam",
+    # 16GB end
+    # "--learning_rate", "5e-6", #1, 2 번째 실습
+    "--learning_rate", "1e-6",
     "--lr_scheduler", "constant",
     "--lr_warmup_steps", "0",
     "--num_class_images", "200",
-    "--max_train_steps", "800",
+    "--num_train_epochs", "100"
+    # "--max_train_steps", "800",
 ]
 
 # OS별 명령어 포맷팅 (Windows와 리눅스에서 모두 작동)
 if os.name == 'nt':
     command = ['cmd', '/c'] + command
-else:
-    command = ['bash', '-c', ' '.join(command)]
 
 # 명령어 실행
 subprocess.run(command)
